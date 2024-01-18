5a12f84561b93f6f73ee382dfe901d4494a022b1
Kimjihun <you@example.com>
2024-01-18 17:48:51 +0900

    ã자바 기초

---
+++ src/ex01/Example02.java
@@ -0,0 +1,30 @@
+package ex01;
+
+
+//default 접근 제한
+class Ex01 {
+
+    //public 접근 제한
+    public void Ex02() {
+    }
+
+    //private 접근제한
+    private void Ex03() {
+    }
+
+    //defalut 접근제한
+    void Ex04() {
+    }
+
+    //protected 접근제한
+    protected void Ex05() {
+    }
+
+}
+
+
+public class Example02 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex01/voidEx.java
@@ -0,0 +1,21 @@
+package ex01;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+class Ex1 {
+    public static void printHello() {
+        System.out.println("Hello, world");
+    }
+
+    private final static List<String> name = Arrays.asList("Mango", "Tistory");
+
+}
+
+public class voidEx {
+    public static void main(String[] args) {
+
+
+    }
+}
