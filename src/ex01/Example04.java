6659d10c480e1834d5a45d5c0b170268b3f37042
김지훈 <greenpig4b@gmail.com>
2024-01-21 23:27:00 +0900

    Example 수정본

---
+++ src/ex01/Example04.java
@@ -0,0 +1,15 @@
+package ex01;
+
+class ex22{
+    public String  as;
+
+}
+public class Example04 {
+
+    public static void main(String[] args) {
+        ex22 er = new ex22();
+        System.out.println(er.as);
+
+    }
+
+}
